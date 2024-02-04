1abbaa879ffc418c1258510ff4327cf11cacc707
Monita <mitramonita@gmail.com>
2024-02-03 22:49:29 -0600

    reverted back to original idea

--- script.js
+++ script.js
@@ -2,25 +2,20 @@
 const btnStartElement = document.querySelector('[data-action="start"]');
 const btnStopElement = document.querySelector('[data-action="stop"]');
 const btnPauseElement = document.querySelector('[data-action="pause"]');
-const btnBreakElement = document.querySelector('[data-action="break"]');
 const hours = document.querySelector('.hours');
 const minutes = document.querySelector('.minutes');
 const seconds = document.querySelector('.seconds');
-const break_dropdown = document.querySelector('.break_minutes_div');
 let timerTime = 0;
-let savedTime = 0;
-let breakTime = 0;
 let interval;
 let pauseTime;
 let userDurationHours, userDurationMinutes;
 let userInputHours, userInputMinutes;
 let isPaused = false;
-let isBreak = false;
 
 const timer = document.getElementById('timer');
 
 // want to exit the break, want to start the break
-const startFocus = () => {
+const start = () => {
   if (isPaused) {
     isPaused = false;
     timerTime = pauseTime;
@@ -59,66 +54,6 @@ const startFocus = () => {
     }
     if (btnPauseElement.classList.contains("hidden")) {
         btnPauseElement.classList.remove("hidden");
-    }
-    if (btnBreakElement.classList.contains("hidden")) {
-        btnBreakElement.classList.remove("hidden");
-    }
-
-    // show the timer
-    timer.classList.remove("hidden");
-
-    // display starting time
-    hours.innerText = pad(userDurationHours);
-    minutes.innerText = pad(userDurationMinutes);
-    
-    // decrement the timer every second starting right away
-    decrementTimer();
-    interval = setInterval(decrementTimer, 1000);
-  }
-}
-
-const startBreak = () => {
-  if (isPaused) {
-    isPaused = false;
-    timerTime = pauseTime;
-    btnStartElement.classList.add("hidden");
-    btnPauseElement.classList.remove("hidden");
-    break_dropdown.classList.add("hidden");
-  } else {
-    // get user input and parse
-    // also make user input box go away once started
-    userInputHours = document.getElementById("timeHours");
-    userInputHours.classList.add("hidden");
-    if (userInputHours.value == "") {
-            userInputHours.value = 0;
-        }
-    userDurationHours = parseInt(
-        document.getElementById("timeHours").value);
-
-    userInputMinutes = document.getElementById("timeMinutes");
-    userInputMinutes.classList.add("hidden");
-    if (userInputMinutes.value == "") {
-            userInputMinutes.value = 0;
-        }
-    userDurationMinutes = parseInt(
-        document.getElementById("timeMinutes"). value);
-
-    // set starting seconds
-    timerTime = (userDurationHours * 3600) + (userDurationMinutes * 60);
-    // }
-
-    // hide start and show other buttons
-    btnStartElement.classList.add("hidden");
-
-    // TODO: make more efficienct
-    if (btnStopElement.classList.contains("hidden")){
-        btnStopElement.classList.remove("hidden");
-    }
-    if (btnPauseElement.classList.contains("hidden")) {
-        btnPauseElement.classList.remove("hidden");
-    }
-    if (btnBreakElement.classList.contains("hidden")) {
-        btnBreakElement.classList.remove("hidden");
     }
 
     // show the timer
@@ -141,10 +76,8 @@ const stop = () => {
   btnStartElement.classList.remove("hidden");
   btnStopElement.classList.add("hidden");
   btnPauseElement.classList.add("hidden");
-  btnBreakElement.classList.add("hidden");
   userInputHours.classList.remove("hidden");
   userInputMinutes.classList.remove("hidden");
-  break_dropdown.classList.add("hidden");
   timer.classList.add("hidden");
 }
 
@@ -162,19 +95,6 @@ const pause = () => {
   // manipulate timer time
   btnStartElement.classList.remove("hidden");
   btnPauseElement.classList.add("hidden");
-  btnBreakElement.classList.add("hidden");
-}
-
-// take break same as pause
-const take_break = () => {
-  // pause();
-  isBreak = true;
-  break_dropdown.classList.remove("hidden");
-  btnStartElement.classList.remove("hidden");
-  let minutes = parseInt(document.getElementById("break_minutes").value);
-  breakTime = minutes;
-  savedTime = timerTime;
-  timerTime = minutes;
 }
 
 const pad = (number) => {
@@ -184,10 +104,6 @@ const pad = (number) => {
 const decrementTimer = () => {
   if (!isPaused) {
     if (timerTime <= 0) {
-        if(isBreak) {
-          isBreak = false;
-          timerTime = savedTime;
-        }
         stop();
     }
     const numberHours = Math.floor(timerTime / 3600);
@@ -211,8 +127,4 @@ btnStopElement.addEventListener('click', stopTimer = () => {
 
 btnPauseElement.addEventListener('click', stopTimer = () => {
   pause();
-});
-
-btnBreakElement.addEventListener('click', stopTimer = () => {
-  take_break();
 });
